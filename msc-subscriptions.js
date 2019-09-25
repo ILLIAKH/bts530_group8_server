273a8dfa4b6411ff99bf75a29d5fef76987db1a2
Illia Khomenko <illia.khomenko@gmail.com>
2019-09-25 19:58:14 -0400

    asd

--- msc-subscriptions.js
+++ msc-subscriptions.js
@@ -2,7 +2,6 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 module.exports = new Schema({
-    subId: Number,
     subName: String,
     subPeriod: Number,
     subBoxType: String,
