ce7b20ee00ded1a3726c1c796d7d95a3ac1e941c
ILLIAKH <39228121+ILLIAKH@users.noreply.github.com>
2020-01-28 11:51:23 -0500

    added nodemail from jesica

--- server.js
+++ server.js
@@ -7,6 +7,44 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 const manager = require("./manager.js");
 
+"use strict";
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass // generated ethereal password
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"C-Kiosk" <ckiosk@example.com>', // sender address
+    to: "jessicakrishtul@gmail.com", // list of receivers
+    subject: "Your Subscription Has Changed", // Subject line
+    text: "Hello Jessica! Your fruits subscription has changed. You will now be receiving strawberries, mangos, and bananas!", // plain text body
+    // html: "<b>Hello world?</b>" // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
 const m = manager(
     "mongodb+srv://IKHOMENKO:iphone3G@senecaweb-mymvn.mongodb.net/BTS530?retryWrites=true",
     { useUnifiedTopology: true },
