521f81fe3481b597924857bf63380a410d8afc49
ILLIAKH <39228121+ILLIAKH@users.noreply.github.com>
2020-02-17 00:08:10 -0500

    updated user manager

--- msc-useraccounts.js
+++ msc-useraccounts.js
@@ -18,7 +18,7 @@ module.exports = new Schema({
     country: String,
     postalCode: String,
 
-    pastDeliveries: [String],
+    pastDeliveries: [Subscriptions],
     subscriptionInfo: [Subscriptions],
     //hello
 });
